cef7eb3db72823d5047eb9f8e0f5c1c66844c8a4
Damian <damian.hoogsteder@indicia.nl>
2023-03-20 16:29:34 +0100

    feature: first initialisation

---
+++ Forfeit15.Patchnotes.Api/Controllers/PatchnoteController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Forfeit15.Patchnotes.Api.Controllers;
+
+/// <summary>
+/// Acties rondom patchnotes
+/// </summary>
+[ApiController]
+[Route("[controller]")]
+public class PatchnoteController
+{
+    //Service
+    
+    
+}
