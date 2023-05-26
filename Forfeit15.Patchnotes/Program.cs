8f26dd5e3efd797debf3726debed427997c5aa89
Zephtos <damian.hoogsteder@indicia.nl>
2023-05-26 14:57:41 +0200

    update: messagequeue uri to cloud hosted one

--- Forfeit15.Patchnotes/Program.cs
+++ Forfeit15.Patchnotes/Program.cs
@@ -30,7 +30,7 @@ builder.Services.AddTransient<IPatchnoteService, PatchnoteService>();
 builder.Services.AddTransient<IPatchnoteRepository, PatchnoteRepository>();
 builder.Services.AddTransient<IInfoNodeService, InfoNodeService>();
 builder.Services.AddTransient<IInfoNodeRepository, InfoNodeRepository>();
-builder.Services.AddSingleton<MessageService>(new MessageService("amqp://admin:password@localhost:5672/", "forfeit15-updates"));
+builder.Services.AddSingleton<MessageService>(new MessageService("amqp://lxwoxkvq:tkjW4M8P7fOj-jM7Yu9I4-yTiKYj-yIK@cow.rmq2.cloudamqp.com/lxwoxkvq", "forfeit15-updates"));
 
 builder.Services
     .AddControllers().AddJsonOptions(options =>
